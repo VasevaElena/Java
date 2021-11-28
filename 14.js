28618d1951f4bb459e0427e2755ad808637d5545
Elena Vaseva <aglagla@yandex.ru>
2021-11-28 14:46:13 +0300

    задачи

---
+++ 14.js
@@ -0,0 +1,7 @@
+let second = 1;
+let hour = second * 60;
+let day = hour * 24;
+let month = day * 30;
+alert("В часе" + hour + "секунд");
+alert("В сутках" + day + "секунд");
+alert("В месяце" + month + "секунд");
