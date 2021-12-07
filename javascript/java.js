878a6556da6381113d71b4b96cf86fa62275ea95
Elena Vaseva <aglagla@yandex.ru>
2021-12-07 22:10:26 +0300

    макет

---
+++ javascript/java.js
@@ -0,0 +1,41 @@
+//1 задача
+var num = 3;
+alert(num);
+
+//2 задача
+var a = 10;
+var b = 2;
+alert(+a + b);
+alert(a-b);
+alert(a*b);
+alert(a/b);
+
+//3
+var c = 15;
+var d = 2;
+var result = +c + d;
+alert(result);
+
+//4
+var a = 10;
+var b = 2;
+var c = 5;
+alert(+a + b + c);
+
+//5
+var a = 17;
+var b = 10;
+var c = a - b;
+var d = 7;
+var result = +c + d;
+alert(result);
+
+//6
+var str = "Привет, Мир!";
+alert(str);
+
+//7
+var str1 = "Привет, ";
+var str2 = "Мир!";
+alert(str1 + str2);
+
